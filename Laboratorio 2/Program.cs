644f578b96ca8a3607fb5b505d10571c0d5515a1
Rodriguez-Riochard <rrpinto27@gmail.com>
2025-09-16 19:49:27 -0500

    Laboratorio 2 completo

---
+++ Laboratorio 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            //Ejemplo untilizando las variables de instacia de clase.
+            client.FirstName = "Su_Nombre";
+            client.LastName = "Su_Apellido";
+            client.Age = 15; // Su_Edad
+            client.id = 1;
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+}
+
+public class Client
+{
+    // Declaracion de variables de instancia en clase.
+    public int id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public ushort Age { get; set; }
+
+    public string GetFullName()
+    {
+        //Utilizando las variables de instancia dentro de metodos de la clase.
+        return FirstName + " " + LastName;
+    }
+}
---
+++ Laboratorio 3/Laboratorio 3/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int primerNumero, segundoNumero, suma;
+        Console.WriteLine("introduce el primer numero:");
+        primerNumero = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("introduce el segundo numero:");
+        segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+        suma = primerNumero + segundoNumero;
+
+        Console.WriteLine("la suma de los dos numeros es: " + suma);
+    }
+}
