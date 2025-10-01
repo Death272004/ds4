6d8b4574c882e74c73338b2fb99edea1e6dc876a
Rodriguez-Riochard <rrpinto27@gmail.com>
2025-09-30 19:58:07 -0500

    laboratorio 8

---
+++ Laboratorio 7/Laboratorio 7-2/Dado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Dado
+{
+    private int valor;
+    private static Random aleatorio;
+    l
+    public Dado()
+    {
+        aleatorio = new Random();
+    }
+
+    public void Tirar()
+    {
+        valor = aleatorio.Next(1, 7);  // Números del 1 al 6
+    }
+
+    public void Imprimir()
+    {
+        Console.WriteLine("El valor del dado es:" + valor);
+    }
+
+    public int RetornarValor()
+    {
+        return valor;
+    }
+}
